bd919d734d857551686abde8985a09a1ca5229e4
SagivKatz <sagivkatz1@gmail.com>
2025-07-15 08:02:50 +0000

    Final OAuth flow: added token exchange and Gmail auth

--- gmail_utils.py
+++ gmail_utils.py
@@ -1,7 +1,5 @@
 import os
-import pickle
-from google.auth.transport.requests import Request
-from google_auth_oauthlib.flow import InstalledAppFlow
+import requests
 from googleapiclient.discovery import build
 from dotenv import load_dotenv
 
@@ -9,30 +7,29 @@ load_dotenv()
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
-def authenticate_gmail():
-    creds = None
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
+def exchange_code_for_token(auth_code):
+    token_url = "https://oauth2.googleapis.com/token"
+    client_id = os.getenv("CLIENT_ID")
+    client_secret = os.getenv("CLIENT_SECRET")
+    redirect_uri = os.getenv("REDIRECT_URI")
 
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_config({
-                "installed": {
-                    "client_id": os.getenv("CLIENT_ID"),
-                    "client_secret": os.getenv("CLIENT_SECRET"),
-                    "redirect_uris": [os.getenv("REDIRECT_URI")],
-                    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-                    "token_uri": "https://oauth2.googleapis.com/token"
-                }
-            }, SCOPES)
-            creds = flow.run_local_server(port=0)
+    data = {
+        "code": auth_code,
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "redirect_uri": redirect_uri,
+        "grant_type": "authorization_code",
+    }
 
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+    response = requests.post(token_url, data=data)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception(f"Failed to exchange code: {response.text}")
 
+def authenticate_gmail_with_token(access_token):
+    from google.oauth2.credentials import Credentials
+    creds = Credentials(token=access_token)
     service = build('gmail', 'v1', credentials=creds)
     return service
 
